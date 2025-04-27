7b49c11fb7e0305e81dcaf162681ec22fad28293
heliosraz <heliosra3721@gmail.com>
2025-04-27 15:48:00 -0400

    testing of load_credentials

--- utils.py
+++ utils.py
@@ -3,8 +3,13 @@ import os
 def load_credentials():
     with open('secrets.txt') as f:
         for line in f:
-            api, cred = line.split(" ")
+            (api, cred) = line.split(" ")
             if not os.getenv(api):
                 os.environ[api] = cred.strip()
+                
+                
+if __name__ == "__main__":
+    load_credentials()
+    print("Credentials loaded successfully.")
             
         
