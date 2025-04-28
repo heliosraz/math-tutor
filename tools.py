035c361eed945bdcb05f67779e0a3dee51bbe51e
heliosraz <heliosra3721@gmail.com>
2025-04-28 13:50:47 -0400

    prompt modifications

--- tools.py
+++ tools.py
@@ -20,16 +20,6 @@ from langchain_core.tools import BaseToolkit, Tool
 from typing import List
 from langchain_core.messages import SystemMessage, HumanMessage
 
-SYSTEM_PROMPT = '''
-You are an LaTeX expert. You must only ansgwer the instructions with LaTeX formatted responses. Please respond to the user's queries in a clear and concise manner and never provide any justification. If you are unable to answer the question, please respond with 'I don't know'. You must make sure that your response is able to compile using MathJax
-
-###LaTeX Libraries
-You may use the following LaTeX libraries in your response:
-amsmath
-amssymb
-tikz
-pgfplots
-'''
 
 # class MathToolkit(BaseToolkit):
 #     _model: str 
@@ -92,7 +82,16 @@ class LaTeXToolkit(BaseToolkit):
             temperature=0.0,
             verbose=True,
         )
-        self._system_prompt = SYSTEM_PROMPT
+        self._system_prompt = '''
+You are an LaTeX expert. Your only job is to format input in LaTeX and not to answer questions. You must only answer the instructions with LaTeX formatted responses. Please respond to the user's queries in a clear and concise manner and never provide any justification. If you are unable to answer the question, please respond with 'I don't know'. You must make sure that your response is able to compile using MathJax. You must only return the equations in LaTeX format and not the solution.
+
+###LaTeX Libraries
+You may use the following LaTeX libraries in your response:
+amsmath
+amssymb
+tikz
+pgfplots
+'''
 
     def _invoke_model(self, prompt: str) -> str:
         return self._model.invoke([
@@ -103,14 +102,9 @@ class LaTeXToolkit(BaseToolkit):
     def get_tools(self) -> List[Tool]:
         return [
             Tool(
-                name="generate_equation",
-                func=lambda problem_reasoning: self._invoke_model(f"Generate the equation for this following description: {problem_reasoning}"),
-                description="Generates an equation for a given description."
-            ),
-            Tool(
                 name="format_equation",
-                func=lambda equation: self._invoke_model(f"Format the equation: {equation}"),
-                description="Format the given equation into LaTeX format."
+                func=lambda equation: self._invoke_model(f"Format any number of equations that would be helpful for solving this problem: {equation}."),
+                description="Extracts and formats equation the given equation into LaTeX format."
             ),
             Tool(
                 name="format_plot",
@@ -119,40 +113,57 @@ class LaTeXToolkit(BaseToolkit):
             )
         ]
 
-# @tool
-# def generate_equation(problem_reasoning:str):
-#     """Generates an equation for a given description"""
-#     prompt = f"Generate the equation for this following description: {problem_reasoning}"
-#     return model.invoke([SystemMessage(content=SYSTEM_PROMPT),HumanMessage(content=prompt)])
-
-# @tool
-# def format_equation(equation:str):
-#     """Format the given equation into LaTeX format."""
-#     prompt = f"Format the equation: {equation}"
-#     return model.invoke([SystemMessage(content=SYSTEM_PROMPT),HumanMessage(content=prompt)])
-    
-# @tool
-# def format_plot(equation:str):
-#     """Plot the given equation in a LaTeX format."""
-#     prompt = f"Plot this equation: {equation}"
-#     return model.invoke([SystemMessage(content=SYSTEM_PROMPT),HumanMessage(content=prompt)])
-
-# @tool
-# def plan(problem_reasoning:str):
-#     """Plan the given problem reasoning"""
-#     prompt = f"Plan the following problem reasoning: {problem_reasoning}"
-#     return model.invoke([SystemMessage(content=SYSTEM_PROMPT),HumanMessage(content=prompt)])
-
-# @tool
-# def solve(problem_reasoning:str):
-#     """Solve the given problem reasoning"""
-#     prompt = f"Solve the following problem reasoning: {problem_reasoning}"
-#     return model.invoke([SystemMessage(content=SYSTEM_PROMPT),HumanMessage(content=prompt)])
 
 
 
+class PlanningToolkit(BaseToolkit):
+    _model: str 
+    _system_prompt: str 
+    def __init__(self, model_name: str, **kwargs):
+        super().__init__(**kwargs)
+        self._model = ChatTogether(
+            model=model_name,
+            max_tokens=512,
+            temperature=0.0,
+            verbose=True,
+        )
+        self._system_prompt = '''
+You are an curriculum expert. Your job is to make a plan of answering and solving the student's question, without fully answering the problem. Please consider what. Please respond to the user's queries in a clear and concise manner and provide justification if needed. If you are unable to answer the question, please respond with 'I don't know'. 
 
-# class PlanningToolkit(BaseToolkit):
+###Consider:
+- The student's question
+- The student's level of understanding
+- The student's learning goals
+- The student's confusion
+
+###Topics
+These are the following topics students may ask about and are expected to know:
+- Algebra
+- Geometry
+- Trigonometry
+- Probability
+- Statistics
+'''
+
+    def _invoke_model(self, prompt: str) -> str:
+        return self._model.invoke([
+            SystemMessage(content=self._system_prompt),
+            HumanMessage(content=prompt)
+        ])
+
+    def get_tools(self) -> List[Tool]:
+        return [
+            Tool(
+                name="plan",
+                func=lambda problem: self._invoke_model(f"Generate a plan to solve the following problem: {problem}"),
+                description="Generates a plan to the solution for a given problem description."
+            ),
+            Tool(
+                name="elaborate",
+                func=lambda confusion, problem: self._invoke_model(f"Generate a plan to address the student's confusion for the problem.\n Confusion: {confusion}\n Problem: {problem}"),
+                description="Generates a plan to address the student's confusion for the problem."
+            )
+        ]
 #     @tool
 #     def tavily_tool(self):
 #         """Creating the tavily tool"""
