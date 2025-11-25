f39ab7b82e6d438d9eb4b46313ce10590852b1fb
heliosraz <heliosra3721@gmail.com>
2025-11-24 19:42:26 -0800

    changed the tool-call-parser to match documentation

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@ vllm serve Qwen/Qwen3-0.6B --max_num_batched_tokens 37376 \
                         --max_model_len 37376 \
                         --enforce-eager \
                         --enable-auto-tool-choice \
-                        --tool-call-parser minimax
+                        --tool-call-parser hermes
