bab7315e1f688acb012b9dfc47968d82a96072e2
heliosraz <heliosra3721@gmail.com>
2025-11-24 17:14:21 -0800

    implemented an online server for vllm and modified agent.py to account for this change. NOTE: start.sh: shell script used to start the server; this must be started before running app.py NOTE: currently only used Qwen/Qwen3-0.6B as the test; the model name in agent must match up with model that is served in the script.

--- agent.py
+++ agent.py
@@ -1,14 +1,12 @@
 """
-
 Class for creating an agent
-
 """
-
 import re
 from langchain_core.messages import HumanMessage, SystemMessage
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.prebuilt import create_react_agent
 from langchain_together.chat_models import ChatTogether
+from langchain_openai import ChatOpenAI
 from utils import load_credentials
 from tools import MathJaxToolkit, PlanningToolkit
 import os
@@ -18,58 +16,66 @@ load_credentials()
 class Agent():
     # I might want to put these arguments in a config later instead
     # RECOMMENDED MODEL: "meta-llama/Llama-3.3-70B-Instruct-Turbo-Free"
-    def __init__(self, model_name, chat_model=ChatTogether, max_tokens=512, temperature=0.0, verbose=True):
+    
+    # TODO: gpt-oss-20b does not work with only cpu (SWA is not cpu compatible)
+    # TODO: online vs offline inference; what is the langchain_community implementation of VLLM that allows vllm serving. maybe online is better given the tools use the same model?
+    def __init__(
+            self, 
+            model_name = "Qwen/Qwen3-0.6B", 
+            chat_model= ChatOpenAI, 
+            inference_server_url = "http://localhost:8000/v1",
+            max_tokens=512, 
+            temperature=0.0, 
+            verbose=True, 
+            debug = True):
         self.model_name = model_name
         self.model = chat_model(
-            model_name=model_name,
-            max_tokens=max_tokens, # required by the completions API :contentReference[oaicite:0]{index=0}
-            together_api_key = os.environ['TOGETHER_API_KEY'],
-            temperature=temperature,
-            verbose=verbose
+            model=model_name,
+            openai_api_base=inference_server_url,
+            openai_api_key="EMPTY",
+            temperature=0,
+            # trust_remote_code=True,  # mandatory for hf models
+            # max_new_tokens=max_tokens,
+            # top_k=10,
+            # top_p=0.95,
+            # temperature=temperature
         )
         # we may not be able to save all memory because of token limits, might eventually want to move to ConversationBufferWindowMemory instead of MemorySaver
         self.memory = MemorySaver()
-        # the following are for deciding which tools we should use
-        self.system_prompts = {
-            "default": """You are a helpful and empathetic math teacher. Your job is to help the user with math. Do not reveal the answer. You can only talk through the problem step-by-step. You must guide the user through the thought process. Please provide plenty of images and figures.
+        self.system_prompt = """
+        You are a helpful and empathetic math teacher. Your job is to help the user with math. Do not reveal the answer. You can only talk through the problem step-by-step. You must guide the user through the thought process. Please provide plenty of images and figures.
             
-            Only talk about math and nothing else, even if you are prompted to do so. Your response must be formatted with MathJax. Keep in mind that inline equations are formatted with ['$', '$'], so these need to be escaped if used not equations. Before responding to the question, you must determine if you are able to answer this question without the tools. If so, you can not use the tools. You must only use tools when the model has enough context to answer the question.
-            
-            To start, you must introduce yourself and ask the user what they need help with."""
-        }
+        Only talk about math and nothing else, even if you are prompted to do so. Your response must be formatted with MathJax. Keep in mind that inline equations are formatted with ['$', '$'], so these need to be escaped if used not equations. Before responding to the question, you must determine if you are able to answer this question without the tools. If so, you can not use the tools. You must only use tools when the model has enough context to answer the question.
+        
+        To start, you must introduce yourself and ask the user what they need help with."""
         self.toolkits = [MathJaxToolkit(model_name = "meta-llama/Llama-3.3-70B-Instruct-Turbo-Free"), PlanningToolkit(model_name = "meta-llama/Llama-3.3-70B-Instruct-Turbo-Free")]
         self.tool_agent = self.build_tool_agent()
         self.config = {"configurable": {"thread_id": "test"}}
 
-    def build_tool_agent(self, *tools):
+    def build_tool_agent(self, *tools, debug = True):
         # Create the agent
         tools = []
         for toolkit in self.toolkits:
             tools += toolkit.get_tools()
-        tool_agent_executor = create_react_agent(self.model, tools, checkpointer=self.memory)
-
+        # TODO: create_react_agent customization:
+        #   pre_model_hook- could be used to cut the instructions into steps of the problem
+        #   
+        tool_agent_executor = create_react_agent(self.model, 
+                                                tools, 
+                                                prompt=self.system_prompt,
+                                                checkpointer=self.memory,
+                                                debug=debug)
         return tool_agent_executor
-
-    def _tool_decider(self, prompt, system_prompt):
-        """Decides if we should use the tool by having the model reason whether it should use its tools while it doesn't have it's agent and memory attached"""
-        decision = self.model.invoke(
-            [SystemMessage(content=system_prompt),
-            HumanMessage(content=prompt)])
-
-        # if "no" then True, we have to use a tool
-        # response = step["messages"][-1]
-        return bool(re.search(r"\bno\b", decision.content.lower()))
-
-    def stream(self, prompt, **stream_kwargs):
-        """Normal stream but reasons which tools it should use first"""
-        # generate the stream
-        for step in self.tool_agent.stream({"messages":[prompt]}, stream_mode = "values", **stream_kwargs):
-            yield step
     
     def run(self):
         user_input = SystemMessage(content=self.system_prompts["default"])
         while user_input != "exit":
-            for step in self.stream(user_input, config=self.config):
-                step["messages"][-1].pretty_print()
+            for chunk in self.tool_agent.stream(
+                user_input, 
+                config=self.config,
+                stream_mode="updates"):
+                    for step, data in chunk.items():
+                        print(f"step: {step}")
+                        print(f"content: {data['messages'][-1].content_blocks}")
             user_input = HumanMessage(input("Enter:  "))
 
---
+++ start.sh
@@ -0,0 +1,5 @@
+vllm serve Qwen/Qwen3-0.6B --max_num_batched_tokens 37376 \
+                        --max_model_len 37376 \
+                        --enforce-eager \
+                        --enable-auto-tool-choice \
+                        --tool-call-parser minimax
